c521b595f539180500b58194130cea1d726df784
Juanfrandouk <61378658+Juanfrandouk@users.noreply.github.com>
2023-05-22 13:25:17 +0200

    Adding Data Notations

--- API_BlogPlatform/Domain/Models/BlogPost.cs
+++ API_BlogPlatform/Domain/Models/BlogPost.cs
@@ -1,4 +1,7 @@
-﻿namespace API_BlogPlatform.Domain.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API_BlogPlatform.Domain.Models
 {
     /// <summary>
     /// Main class in  model
@@ -6,9 +9,14 @@
     public class BlogPost
     {
         public int Id { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(100)")]
         public string Title { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(200)")]
         public string Content { get; set; }
-
+        [Required]
+        [Column(TypeName = "varchar(200)")]
         public string Author { get; set; }
 
     }
